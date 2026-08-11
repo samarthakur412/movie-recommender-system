bc94688cca56b299e9f52b195fed083839bb6eff
Samar <123291990+samarthakur412@users.noreply.github.com>
2026-08-12 00:00:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,10 +58,10 @@ st.markdown("""
 
 # -------------------- LOAD DATA --------------------
 
-movies_dict = pickle.load(open('movies_dict.pkl','rb'))
+movies_dict = pickle.load(open('data/processed/movies_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 
-similarity = pickle.load(open('similarity.pkl','rb'))
+similarity = pickle.load(open('data/processed/similarity.pkl', 'rb'))
 
 
 
