eb619d66951c5f524aa670076f2d60fadb4b2fc4
Samar <123291990+samarthakur412@users.noreply.github.com>
2026-08-11 23:58:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
-from config import config
 
 # API_KEY = config['api_key']
 # NUM_RECOMMENDATIONS = config['num_recommendations']
